f46de845da4deb5e3fe7916e5fe1a0fb5ce9477f
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-06 16:46:26 +0200

    added ingredients to detail view

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
     public name: string;
     public desription: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name:string,desription:string,imagePath:string){
+    constructor(name:string,desription:string,imagePath:string, ingredients:Ingredient[]){
         this.name = name;
         this.desription = desription;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
