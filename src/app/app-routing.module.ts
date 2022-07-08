dc2ee7148a5c8afdd44a819f32fde958f8e0f71a
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-08 15:47:35 +0200

    routing (3/3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule, Routes } from "@angular/router";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesDetailComponent } from "./recipes/recipes-detail/recipes-detail.component";
 import { RecipesComponent } from "./recipes/recipes.component";
@@ -7,12 +8,12 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'redipes', pathMatch: 'full' },
-    {
-        path: 'recipes', component: RecipesComponent, children: [
+    { path: 'recipes', component: RecipesComponent, children: [
             { path: '', component: RecipeStartComponent },
-            { path: ':id', component: RecipesDetailComponent }
-        ]
-    },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipesDetailComponent },            
+            { path: ':id/edit', component: RecipeEditComponent },
+        ] },
     { path: 'shopping-list', component: ShoppingListComponent }
 
 ]
--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -13,7 +13,8 @@ export class RecipesDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService:RecipeService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -26,6 +27,12 @@ export class RecipesDetailComponent implements OnInit {
 
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    console.log("Route ", this.route);
+    //this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 
 }
