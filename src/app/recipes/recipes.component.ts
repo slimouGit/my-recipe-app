ceeae0e523ed68a1bd5c836ce979c5f8a4615426
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 10:15:33 +0200

    using subject instead of emitter

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 
@@ -11,16 +10,10 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
   
-  selectedRecipe: Recipe;
-
-  constructor(private recipeService:RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe:Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
+  
   }
 
 }
