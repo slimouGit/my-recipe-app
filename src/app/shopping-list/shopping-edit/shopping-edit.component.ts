1d68a5c2574ae3603790e8ba06c50308988a2334
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 15:58:07 +0200

    delete items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -54,4 +54,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
 }
