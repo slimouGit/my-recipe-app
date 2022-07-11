05ba9b9bdbde53d4cf45fc78c22b9e63268c7b35
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 18:52:12 +0200

    delete ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -102,5 +102,8 @@ export class RecipeEditComponent implements OnInit {
       this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  onDeleteIngredient(index:number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 
 }
