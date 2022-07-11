7e5833b0c146c6c73b3a239e4ab60102426c2158
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 18:48:00 +0200

    delete, save and update recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -15,7 +15,7 @@ export class RecipeEditComponent implements OnInit {
   recipe: Recipe;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeServie: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeServie: RecipeService, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -83,6 +83,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeServie.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -95,6 +96,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     )
+  }
+
+  onCancel(){
+      this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 
